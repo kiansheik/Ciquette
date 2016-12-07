1d05a8210bbe2b1b1a3f8b33a367dd1c828e884f
kiansheik <kasheik@dons.usfca.edu>
2016-12-06 23:47:29 -0800

    Many updates
    Mainly made it so that you can create visualization from table

--- instantvis/valsnatch.js
+++ instantvis/valsnatch.js
@@ -1,13 +1,31 @@
-
 if (window == top) {
     var t = '';
-    var pool = chrome.runtime.connect({name:"poop"})
-    pool.onDisconnect.addListener(function(){console.log("NOOOO")})
+    var pool = chrome.runtime.connect({ name: "pool" })
+
     function gText(e) {
         t = (document.all) ? document.selection.createRange().text : document.getSelection().toString();
-
-        pool.postMessage(t);
-        console.log(t)
+        m = t.trim().match(/[^\r\n]+/g).map(function(str){return str.split(/\t+/g)})
+        console.log("bef", t)
+        console.table(m)
+        if (m.length == 1) {
+            var tsplit = t.split(/\s+/g)
+            pool.postMessage({load:tsplit,label:Array.apply(null, {length: tsplit.length}).map(Number.call, Number+1)});
+            console.table(t.split(/\s+/g));
+        } else if (m.length >= 2) {
+            var load = [],
+                label = [];
+            var start = m[0].length;
+            var end = m[m.length - 1].length - 1;
+            for (var i = 0; i < m.length-1; i++) {
+                label.push(m[i][m[i].length-start]);
+                load.push(m[i][end]);
+            }
+            label.push(m[m.length-1][m[m.length-2].length-m[0].length]);
+            load.push(m[m.length-1][end]);
+            
+            pool.postMessage({load:load, label:label})
+            console.table({label:label, load:load})
+        }
     }
 
     document.onmouseup = gText;
